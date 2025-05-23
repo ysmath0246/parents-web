4a9572255e3278986fb0f7e5f5224374045c0bd5
ysmath0246 <heeju060689@gmail.com>
2025-05-23 21:27:08 +0900

    로그인이안되

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' 
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
    <StrictMode>
-     <BrowserRouter>   
+    {/* GitHub Pages 서브폴더용 basename 설정 */}
+   <BrowserRouter basename="/parents-web">   
            <App />         
-    </BrowserRouter>
+   </BrowserRouter>
   </StrictMode>,
 )
