fa3ec8f7783538cdbbf10da10170edb62cf24c0c
ysmath0246 <heeju060689@gmail.com>
2025-05-23 21:30:23 +0900

    로그인이

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', 
+   // GitHub Pages 서브폴더용으로 바꿉니다
+  base: '/parents-web/',
   plugins: [react()],
   optimizeDeps: {
     // 기존 exclude는 그대로 두시고,
